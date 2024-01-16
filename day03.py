481e4bd3a2e6b4f2cc3399564d1b9f0345960ff7
white9812 <white9812@gmail.com>
2024-01-16 14:39:56 +0900

    v2.1

--- day03.py
+++ day03.py
@@ -1,17 +1,20 @@
 
 # prime number
 number=int(input("Input number : "))
-is_prime=True # 공간을 줄임 int -> bool
-
-i=2
-while i< number: #1과 자기자신은 돌지않음
-    if number % i == 0:
-       if_prime = False #remove +
-        break
-    i+=1
-
-#if cnt ==0: #remove ==
-if is_prime:
-    print(f"{number} is prime number")
-else:
+is_prime=True
+if number <2 :
     print(f"{number} is NOT prime number")
+else:
+
+    i=2
+    while i< number:
+        if number % i == 0:
+            if_prime = False
+            break
+        i=i+1
+
+
+    if is_prime:
+        print(f"{number} is prime number")
+    else:
+        print(f"{number} is NOT prime number")
