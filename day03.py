9ef6947778b357758d767c5570447e04d903de3c
white9812 <white9812@gmail.com>
2024-01-16 14:33:20 +0900

    v1.9

--- day03.py
+++ day03.py
@@ -7,8 +7,10 @@ i=2
 while i< number: #1과 자기자신은 돌지않음
     if number % i == 0:
         cnt=cnt+1
+        break #한번이라도 들어오면 끝남
         #print(i) 약수 구하기
     i+=1 #무한 루프에 빠지지않기위해
+    print(i,end=" ")
 if cnt ==0:
     print(f"{number} is prime number")
 else:
@@ -18,3 +20,20 @@ else:
 #0의 의미=subjects format안에 첫번째 인수라는 것 응용하면 dictionary 여러개 쓸 수 있음
 #소수
 #count변수하나 저장하고 나누어떨어지는 횟수가 2번이면 소수
+#약수가 발견되는 시점에 break문으로 탈출하면 됨
+number=int(input("Input number : "))
+cnt=0
+
+i=2
+while i< number: #1과 자기자신은 돌지않음
+    if number % i == 0:
+        cnt=cnt+1
+
+
+   #print(i) 약수 구하기
+    i+=1 #무한 루프에 빠지지않기위해
+    print(i,end=" ")
+if cnt ==0:
+    print(f"{number} is prime number")
+else:
+    print(f"{number} is NOT prime number")
