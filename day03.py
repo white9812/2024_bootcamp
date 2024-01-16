561dcc2a960d0a7cd7ebae16daa2bcc7d2383cc3
white9812 <white9812@gmail.com>
2024-01-16 18:44:16 +0900

    v2.6 hw 2

--- day03.py
+++ day03.py
@@ -1,51 +1,32 @@
+#1
+for i in range(3,-1,-1):
+    print(i)
+
+#2
+guess_me= 7
+number=1
 while True:
-    menu = input("1) Fahraenheit ->Celsius 2) Celsius -> Fahrenheit 3)one prime number :       "
-                 "4) prime numbers:           5) Quit program"    )
-    if menu == "1":
-        fahrenheit = float(input("Input Fahreheit : "))
-        print(f"fahrenheit: {fahrenheit}F,Celsius: {((fahrenheit - 32.0) * 5.0 / 9.0):.4f}C")
-    elif menu == "2":
-        celsius = float(input("Input celsius : "))
-        print(f"fahrenheit: {fahrenheit}F,Celsius: {(celsius * (9.0 / 5.0) + 32):.4f}C")
-    elif menu == "3":
-        number = int(input("Input number : "))
-        is_prime = True
-        if number < 2:
-            print(f"{number} is NOT prime number")
-        else:
+    if guess_me > number:
+        print("too low")
+    elif guess_me < number:
+        print("oops")
 
-            for i in range(2, number):
-                if number % i == 0:
-                    if_prime = False
-                    break
-                # i=i+1 for문 때문에 자동 따라서 제거한다.
+        break
+    else:
+        print("find it!")
+        print(number)
+        break
 
-            if is_prime:
-                print(f"{number} is prime number")
-            else:
-                print(f"{number} is NOT prime number")
-
-    elif menu == "4":
-        numbers = input("Input number : ").split()  # list
-        n1 = int(numbers[0])
-        n2 = int(numbers[1])
-
-        if n1 > n2:
-            n1, n2 = n2, n1
-        for number in range(n1, n2 + 1):
-            is_prime = True
-
-            if number < 2:
-                # pass
-                continue  # 둘다 해도 된다. 하지만 아무것도 안쓰면 문법 에러난다.
-            else:
-                for i in range(2, number):
-                    if number % i == 0:
-                        is_prime = False
-                        break
-                # i=i+1 for문 때문에 자동 따라서 제거한다.
-                if is_prime: print(number, end=" ")
-
-    elif menu == "5":
-        print("프로그램 종료합니다.")
+    number = number + 1
+#3
+guess_me=5
+for number in range(10):
+    if number < guess_me:
+        print("too low")
+    elif (number ==
+          guess_me):
+        print("find it!")
+        break
+    else :
+        print("oops")
         break
