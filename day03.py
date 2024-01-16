48c990b1a7473de8651d19833b89dc0a2e9c75e1
white9812 <white9812@gmail.com>
2024-01-16 15:02:58 +0900

    v2.3 prime number (for)

--- day03.py
+++ day03.py
@@ -6,12 +6,11 @@ if number <2 :
     print(f"{number} is NOT prime number")
 else:
 
-    i=2
-    while i< number:
+ for i in range(2,number)
         if number % i == 0:
             if_prime = False
             break
-        i=i+1
+        #i=i+1 for문 때문에 자동 따라서 제거한다.
 
 
     if is_prime:
