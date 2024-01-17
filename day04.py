ede91dae416525e4675b73fd04a57a84b1b96cdb
white9812 <white9812@gmail.com>
2024-01-17 13:34:11 +0900

    v0.5  list

--- day04.py
+++ day04.py
@@ -1,27 +1,23 @@
-t9=1,21,3
-t10=1,2,3,4
-print(t9>t10)#순서대로 하나씩 비교
-subjects=["c++","Java","python"]
-print(subjects[::-1])
-print(subjects)#원본이 바뀌진 않았음 할당을 한것은 아니라서
-subjects.reverse()#원본자체를 바꿈
-print(subjects)
-subjects=subjects[::-1]
-print(subjects[::-1])
-subjects=["c++","Java","python","Java"]
+#subjects=["a","b","c"]
+subjects=["a",["b","c"],"d"]
+a=subjects
+b=subjects.copy()
+c=list(subjects)
+d=subjects[:]
+print(subjects,a,b,c,d)
+#subjects[1]="x"
+subjects[1][1]="x"
+print(subjects,a,b,c,d)
 
-#subjects.remove("Java")
-#print(subject)
-#subjects.pop(2)
+import copy
 
-subjects.sort()
-print(subjects)
-subjects.sort(reverse=True)
-print(subjects)
-subjects=["c++","Java","python","Java","5","9"]
-print(subjects)
-subjects.sort()
-print(subjects)
-subjects=["c++",5,"Java","python",1,"Java"]
-a=[1,2,3]
-b=a.copy()
+#subjects=["a","b","c"]
+subjects=["a",["b","c"],"d"]
+a=subjects
+b=copy.deepcopy(a)
+c=list(subjects)
+d=subjects[:]
+print(subjects,a,b,c,d)
+#subjects[1]="x"
+subjects[1][1]="x"
+print(subjects,a,b,c,d)
