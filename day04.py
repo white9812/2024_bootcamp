c2df5ba7eb52af012e6e7e13e63f025db9187c62
white9812 <white9812@gmail.com>
2024-01-17 10:35:33 +0900

    v0.3 tuple

--- day04.py
+++ day04.py
@@ -1,25 +1,33 @@
-numbers = input("Input number : ").split()  # list
-n1 = int(numbers[0])
-n2 = int(numbers[1])
+t1=(5)#이건 tuple이 아님. 그냥 integer에 소괄호가 쓰여져있음
+t2=5,
+t3=(5,)
+t4=(5,7)
+t5=5,7
+print(type(t1),type(t2),type(t3),type(t4),type(t5))
+#소괄호를 쓰지않아도 tuple을 만들 수 있다.
+t6="python","kim" #packing -,사용하면 담는 역할을 함,
+print(type(t6),t6[1])
+#2개를 동시에 담을 수 있음
+subject,prof =t6
+print(prof) #unpacking, 순서대로 subject,prof에 들어감 주의사항:갯수가 맞아야됨
+#a,b,c=t6 #ValueError: not enough values to unpack (expected 3, got 2)
+print(subject)
+t7=()
+t8=tuple()
+print(type(t7),type(t8),type(9,),type((9,)))#type(9,)에서 ,는 type의 부분으로 인식
+t9=1,2,3
+t10=1,2
+print(t9==t10)
+print(t9 <= t10)
+print(t9 < t10)
+print(t9>t10)
+t11=4.43, #tuple로 만들려면 입력값 하나일때 ,필수
+t12=3.97,4.1,3.27
+print(id(t11))
+print(t11+t12)
 
-if n1 > n2:
-    n1, n2 = n2, n1
-for number in range(n1, n2 + 1):
-    is_prime = True
 
-    if number < 2:
-         # pass #1은 계산할 필요가 없으니까
-        continue  # 둘다 해도 된다. 하지만 아무것도 안쓰면 문법 에러난다.
-    else: #소수가 아닐때 계속 돌아간다.
-        i=2
-        while i*i <= number: #그냥 i를 i*i로 하였음 소수가 아닌 수도 처리했지만 소수인 수도
-                             #소수인 수도 반복문의 횟수를 줄임
+t11=t11+t12
+print(t11)
+print(id(t11))
 
-            if number % i == 0:
-                is_prime = False
-                break
-            i=i+1 #for문 때문에 자동 따라서 제거한다.
-            #print(i) 성능체크하려고 넣은것
-        if is_prime:
-            pass
-            print(number, end=" ")
