9076cb307129e70af17c6c3e63593c381272c8d5
white9812 <white9812@gmail.com>
2024-01-18 11:10:35 +0900

    v0.4  primenumber function(prof)

--- day05.py
+++ day05.py
@@ -1,34 +1,37 @@
-def is_prime(n) -> bool:
-    """
-    매개변수로 넘겨받은 수가 소수인지 여부를 boolean으로 리턴
-    :param n: 판정할 매개변수
-    :return: 소수면 True,아니면 False
-    """
-    #is_prime=True <- 굳이 필요 없음
-    if n<2:
-        return False
-    else:
-        i=2
-        while i*i<=n:
-            if n %i ==0:
-                #is_prime=False
-                #break 함수탈출은 return원래꺼 필요없음
-                return False
-            i+=1
+# prime number with function
+def isprime(n) -> bool:
         return True
 
 
-# help(is_prime)
-# help(len)
+while True:
+    menu = input("1) Fahrenheit -> Celsius   2) Celsius -> Fahrenheit   3) Prime1   4) Prime2   5) Quit program : ")
 
+    if menu == '1':
+        fahrenheit = float(input('Input Fahrenheit : '))
+        print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit-32.0)*5.0/9.0):.4f}C')
+    elif menu == '2':
+        celsius = float(input('Input Celsius : '))
+        print(f'Celsius : {celsius}C, Fahrenheit : {((celsius*9.0/5.0)+32.0):.4f}F')
+    elif menu == '3':
+        number = int(input("Input number : "))
+        if isprime(number):
+            print(f'{number} is prime number')
+        else:
+            print(f'{number} is NOT prime number!')
+    elif menu == '4':
+        numbers = input("Input first second number : ").split()
+        n1 = int(numbers[0])
+        n2 = int(numbers[1])
 
-numbers = input("Input number : ").split()
-n1 = int(numbers[0])
-n2 = int(numbers[1])
+        if n1 > n2:
+            n1, n2 = n2, n1
 
-if n1 > n2:
-    n1, n2 = n2, n1
-for number in range(n1, n2 + 1):
-    if is_prime(number):
-        print(number, end=" ")
-
+        for number in range(n1, n2 + 1):
+            if isprime(number):
+                print(number, end=' ')
+        print()
+    elif menu == '5':
+        print('Terminate Program.')
+        break
+    else:
+        print('Invalid Menu!')
