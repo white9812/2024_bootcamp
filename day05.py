096b5949114e16a9f37b80f68bee56196360fd56
white9812 <white9812@gmail.com>
2024-01-18 10:52:13 +0900

    v0.2 primenumber function

--- day05.py
+++ day05.py
@@ -1,8 +1,42 @@
-univ="inha university"
-counts_alphabet={letter:word.count(letter) for letter in univ}
-print(counts_alphabet)
+def is_prime(n) -> bool :
+    """
+    매개변수로 넘겨받은 수가 소수인지 여부를 boolean으로 리턴
+    :param n: 판정할 매개변수
+    :return: 소수면 True,아니면 False
+    """
+    #is_prime=True <- 굳이 필요 없음
+    if n<2:
+        return False
+    else:
+        i=2
+        while i*i<=n:
+            if n %i ==0:
+                #is_prime=False
+                #break 함수탈출은 return원래꺼 필요없음
+                return False
+            i+=1
+        return True
 
-counts_alphabet=dict()
-for letter in univ:
-    counts_alphabet[letter]=univ.count(letter)
-print(counts_alphabet)
+
+help(is_prime)
+help(len)
+
+
+numbers = input("Input number : ").split()
+n1 = int(numbers[0])
+n2 = int(numbers[1])
+
+if n1 > n2:
+    n1, n2 = n2, n1
+for number in range(n1, n2 + 1):
+    if is_prime(number):
+        print(number,end=" ")
+    else:
+        for i in range(2, number):
+            if number % i == 0:
+                is_prime = False
+                break #구간소수구할때 그냥 소수 구할때도 썼음
+
+        if is_prime:
+            print(number, end=" ")
+
