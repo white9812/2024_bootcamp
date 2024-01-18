56385fea8e47345a810f433ff5afa40616e75215
white9812 <white9812@gmail.com>
2024-01-18 15:46:28 +0900

    v1.5 decorator

--- day05.py
+++ day05.py
@@ -1,17 +1,60 @@
-def my_range(first=0,last=10,step=1):
-    number=first
-    while number<last:
-        yield number
-        number +=step
+# def my_range(first=0,last=10,step=1):
+#     number=first
+#     while number<last:
+#         yield number
+#         number +=step
+#
+# r=my_range()
+# print(r,type(r))
+#
+# for x in r:
+#     print(x)
+#
+# for x in r:
+#     print(x) #generator걕체 이용했지만 이건 기억되지 않아서 숫자만 발생하고 한번 쓰고 삭제됨 따라서 다시 객체를 만들어야함
 
-r=my_range()
-print(r,type(r))
+# decorator
+def description(f):  # closure
+    def inner(*args):
+        print(f.__name__)
+        print(f.__doc__)
+        r = f(*args)
+        return r
 
-for x in r:
-    print(x)
+    return inner
 
-for x in r:
-    print(x) #generator걕체 이용했지만 이건 기억되지 않아서 숫자만 발생하고 한번 쓰고 삭제됨 따라서 다시 객체를 만들어야함
+
+def squares(n):
+    """
+    제곱 함수
+    """
+    return n * n
+
+@description
+def power(b, e):
+    """
+    거듭제곱 함수
+    """
+    result = 1
+    for _ in range(e):
+        result = result * b
+    return result
+
+
+f1 = description(squares)
+print(f1(9))
+print(power(2, 10))
+# f2 = description(power)
+# print(f2(2, 10))
+
+# print(squares(7))
+# print(squares.__doc__)
+
+# def my_range(first=0, last=5, step=1):
+#     number = first
+#     while number < last:
+#         yield number
+#         number += step
 
 
 
