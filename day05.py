43c291eff84e923cf393ca109ea1e4731d286ad5
white9812 <white9812@gmail.com>
2024-01-18 13:14:43 +0900

    v0.6

--- day05.py
+++ day05.py
@@ -1,50 +1,23 @@
-# prime number with function
-def isprime(n) -> bool:
-    """
-       매개변수로 넘겨 받은 수가 소수인지 여부를 boolean으로 리턴
-       :param n: 판정할 매개변수
-       :return: 소수면 True, 소수가 아니면 False
-       """
-    if n < 2:
-        return False
+# #질문1
+# def a(n1,n2):
+#     print(n1,n2)
+# def a(n):
+#     print(n)
+#
+# a(7)
+# a(7,11)
+# #파이썬은 overloading개념이 없음 자바나 c++은 알아서 입력받은 값의 개수에 따라 알아서 함수 돌림
+# #질문2
+def a(n):
+    if n is None:
+        print(f"{n} is None")
+    elif n:
+        print(f"{n} is True")
     else:
-        i = 2
-        while i * i <= n:
-            if n % i == 0:
-                return False
-            i += 1
-        return True
+        print(f"{n} is False")
+a(None)
+a("")
+a(-9)
+a([])
+a([0])
 
-
-while True:
-    menu = input("1) Fahrenheit -> Celsius   2) Celsius -> Fahrenheit   3) Prime1   4) Prime2   5) Quit program : ")
-
-    if menu == '1':
-        fahrenheit = float(input('Input Fahrenheit : '))
-        print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit-32.0)*5.0/9.0):.4f}C')
-    elif menu == '2':
-        celsius = float(input('Input Celsius : '))
-        print(f'Celsius : {celsius}C, Fahrenheit : {((celsius*9.0/5.0)+32.0):.4f}F')
-    elif menu == '3':
-        number = int(input("Input number : "))
-        if isprime(number):
-            print(f'{number} is prime number')
-        else:
-            print(f'{number} is NOT prime number!')
-    elif menu == '4':
-        numbers = input("Input first second number : ").split()
-        n1 = int(numbers[0])
-        n2 = int(numbers[1])
-
-        if n1 > n2:
-            n1, n2 = n2, n1
-
-        for number in range(n1, n2 + 1):
-            if isprime(number):
-                print(number, end=' ')
-        print()
-    elif menu == '5':
-        print('Terminate Program.')
-        break
-    else:
-        print('Invalid Menu!')
