154907d0e9ce7e21a69fbd7afb823c650371a081
white9812 <white9812@gmail.com>
2024-01-18 14:12:32 +0900

    v1.0 closures

--- day05.py
+++ day05.py
@@ -1,24 +1,19 @@
-# def squares(*n):
-#     return n*n #can't multiply sequence by non-int of type 'tuple'
-def squares(*n) ->list :
-    """
-    넘겨 받은 수치 데이터들의 거듭제곱 값을 리스트에 담아서 리턴
-    :param n: tuple
-    :return: list
-    """
-    return [i*i for i in n]
-# def run_function(f,number):
-#     return f(number) #그냥 f(number)하면 None나옴
-# print(squares(7,5,2))
-# #print(run_function(squares,9))
+# def out_func(nout):
+#     def inner_func(nin):
+#         return nin*nin
+#     return inner_func(nout)
 #
-
- def run_function(f,*number) ->list :
-     return f(*number) #함수를 실현시킨 list
-    #그냥 f(number)하면 None나옴
-print(squares(7,5,2))
-print(run_function(squares,9,10))
+# print(out_func(5)) <- 일반적인 함수 호출
 
 
+#closure는 바깥쪽의 함수의 인수를 그냥 바로 쓴다.
+def out_func(nout):
+    def inner_func():
+        return nout*nout #직접 쓴다.
+    return inner_func
 
+x=out_func(9)
+print(type(x))
+print(x)
+print(x())
 
