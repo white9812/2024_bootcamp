f25cbf0c8fde38f7dd5989afc0e0007874571c1d
white9812 <white9812@gmail.com>
2024-01-18 15:22:20 +0900

    v1.4 generator

--- day05.py
+++ day05.py
@@ -1,10 +1,17 @@
-def squares(n):
-    return n*n
-even_numbers=[i for i in range(51) if i%2==0]
-print(even_numbers)
-print(map(squares,even_numbers))#map(func,sequnce type)
-print(tuple(map(squares,even_numbers)))
+def my_range(first=0,last=10,step=1):
+    number=first
+    while number<last:
+        yield number
+        number +=step
 
-# print(tuple(map(lambda x: x**2 ,even_numbers))) #익명함수 만듦  squares 필요없음, 익명함수도 함수임
-z=lambda x:pow(x,2)
-print(tuple(map(z,even_numbers)))
+r=my_range()
+print(r,type(r))
+
+for x in r:
+    print(x)
+
+for x in r:
+    print(x) #generator걕체 이용했지만 이건 기억되지 않아서 숫자만 발생하고 한번 쓰고 삭제됨 따라서 다시 객체를 만들어야함
+
+
+
