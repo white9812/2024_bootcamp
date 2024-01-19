7e7c8b37ae48428d4e046cd64a0ee3c61708ec6b
white9812 <white9812@gmail.com>
2024-01-19 09:29:15 +0900

    v6.1 9.16.3.1

--- day06.py
+++ day06.py
@@ -1,20 +1,22 @@
-# def good()->list:
-#     '''
-#     chapter 9 things to do 9-1
-#     :return: list
-#     '''
-#     harry_poter=input().split()
-#     return harry_potter
-#
-# print(good())
 
-def get_odds(n):
-    for i in range(1,n+1,2):
-        yield i
-cnt=0
-odds=get_odds(9)
-for odd in odds:
-    cnt=cnt+1
-    if cnt ==3:
-        print(f"Third number is {odd}")
-        break
+#데코레이터니까 인수가 있어야함
+def test(f):
+    '''
+    데코레이터 함수, 함수 시작하면 start출력, 함수 끝나면 end출력
+    :param f: function
+    :return: closure function
+    '''
+    def test_in(*args,**kwargs):#비워서 해도 되지만 필요한 값이 있으면 인수 넣어주기
+        print("start")
+        result=f(*arges,**kwargs)
+        f()
+        print("end")
+        # return result
+    return test_in
+
+def greeting():
+    print("안녕하세요~")
+    #수정하진 않고 확장에는 열려있음
+#인수없으니까 바로 greeting?호출
+t=test(greeting)
+
