1ee45e69cccab13596e4789663c5539b0a3c71a0
white9812 <white9812@gmail.com>
2024-01-19 10:24:37 +0900

    v6.2 recursion

--- day06.py
+++ day06.py
@@ -1,26 +1,24 @@
-#Open closed principale
-#데코레이터니까 인수가 있어야함
-
-
-def test(f):
+#recursion(재귀)
+def factorial_repetition(n)->int:
     '''
-    데코레이터 함수, 함수 시작하면 start출력, 함수 끝나면 end출력
-    :param f: function
-    :return: closure function
+    반복문을 이용한 팩토리얼 함수
+    :param n: 정수,int
+    :return: 팩토리얼 값,int
     '''
-    def test_in(*args,**kwargs):#비워서 해도 되지만 필요한 값이 있으면 인수 넣어주기
-        print("start")
-        result=f(*arges,**kwargs)
-        f()
-        print("end")
-        # return result
-    return test_in
-@test
-def greeting():
-    print("안녕하세요~")
-    #수정하진 않고 확장에는 열려있음
-
-greeting()
-
-#개방폐쇄원칙
-
+    result =1 #곱셈에 대한 연산을 할거라서
+    for i in range(2,n+1):
+        result =result*i #값이 변할 위험성이 있음 따라서 이런경우 재귀함수가 유용 하지만 좀 느림
+    return result
+def factorial_recursion(n):
+    '''
+    재귀함수를 사용한 팩토리얼 함수
+    :param n: 정수,int
+    :return:  function,int
+    '''
+    if n==1:
+        return n
+    else:
+        return n*factorial_recursion(n-1) #n=1이 될때까지 반복
+number=int(input("number: "))
+# print(factorial_repetition(int(input("number : "))))
+print(factorial_recursion(number))
