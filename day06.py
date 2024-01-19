fe90bd5038b0fca33717bd5e235b72c0cd786bcd
white9812 <white9812@gmail.com>
2024-01-19 15:25:36 +0900

    v6.11 property

--- day06.py
+++ day06.py
@@ -6,17 +6,19 @@ class SwimmingMixin:
         return f"{self.name}이(가) 수영을 합니다."
 class Pokemon:
     def __init__(self,name):
-        self.name=name
+        self.hidden_name=name
     def attack(self):
         print("공격!")
 
     def get_name(self):
         print("inside getter")
-        return self.name
+        return self.hidden_name
 
     def set_name(self,new_name):
         print("inside getter")
         self.name=new_name
+
+    name=property(get_name,set_name)
 
 class Charizard(Pokemon,FlyingMixin):
     pass
@@ -31,8 +33,11 @@ c1=Charizard("리자몽")
 # c1.attack()
 #Charizard.attack()#c1안주면 에러남 클래스명을 준것=Charizard.한것 자체가
 #self에는 객체명이 와야함 따라서 c1(self)가 와야함
-print(g1.name) #<-직접적으로 접근
-print(g1.set_name)#g1.setname()하면 에러남
-g1.set_name("잉어킹")
+# print(g1.name) #<-직접적으로 접근
+# print(g1.set_name)#g1.setname()하면 에러남
+# g1.set_name("잉어킹")
+# g1.name="잉어킹"
+#property
+print(g1.name) #property name으로 불러옴
 g1.name="잉어킹"
 print(g1.name)
