eee6982d009137f4b0392d13b54d21ed2885eb7a
white9812 <white9812@gmail.com>
2024-01-19 16:34:47 +0900

    v6.13 hide

--- day06.py
+++ day06.py
@@ -1,23 +1,23 @@
 class FlyingMixin:
     def fly(self):
-        return f"{self.name}이(가) 하늘을 훨훨 날아갑니다~ "
+        return f"{self.__name}이(가) 하늘을 훨훨 날아갑니다~ "
 class SwimmingMixin:
     def swim(self):
-        return f"{self.name}이(가) 수영을 합니다."
+        return f"{self.__name}이(가) 수영을 합니다."
 class Pokemon:
     def __init__(self,name):
-        self.hidden_name=name
+        self.__name=name
     def attack(self):
         print("공격!")
     @property
     def name(self):
         print("inside getter")
-        return self.hidden_name
+        return self.__name
     @name.setter
     def name(self,new_name):
         print("inside getter")
         # self.name=new_name 왜 오류나는지 모르겠음
-        self.hidden_name = new_name
+        self.__name = new_name
 
     # name=property(get_name,set_name)
 
@@ -35,10 +35,20 @@ c1=Charizard("리자몽")
 #Charizard.attack()#c1안주면 에러남 클래스명을 준것=Charizard.한것 자체가
 #self에는 객체명이 와야함 따라서 c1(self)가 와야함
 # print(g1.name) #<-직접적으로 접근
-# print(g1.set_name)#g1.setname()하면 에러남
+# print(g1.set_name)
+# #g1.setname()하면 에러남
+#print(g1.get_name())
 # g1.set_name("잉어킹")
 # g1.name="잉어킹"
 #property
-print(g1.name) #property name으로 불러옴
+# print(g1.name) #property name으로 불러옴
 g1.name="잉어킹"
+g1.hidden_name="잉어킹"
+g1.__name="잉어킹"#에러 값이 안들어감
+g1._Pokemon__name="잉어킹"
+#실질적으로 파이썬에는 접근 제한이 없음 ,접근 제한 하려면 숨기고 싶은 속성에 __하면 됨
 print(g1.name)
+# print(g1.__name) #direct access X
+print(g1.hidden_name)
+print(g1._Pokemon__name) #사실상 private 개념은 없는 걸로 그냥 우회한것
+##__바로 접근 안됨 property직접접근안되니까 .name으로 접근,,?
