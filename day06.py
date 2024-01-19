d723cf8a0cbe05def33e08e92b4542fcfe3dfa2e
white9812 <white9812@gmail.com>
2024-01-19 09:30:31 +0900

    v6.1 9.16.3.2

--- day06.py
+++ day06.py
@@ -1,5 +1,7 @@
-
+#Open closed principale
 #데코레이터니까 인수가 있어야함
+
+
 def test(f):
     '''
     데코레이터 함수, 함수 시작하면 start출력, 함수 끝나면 end출력
@@ -13,10 +15,12 @@ def test(f):
         print("end")
         # return result
     return test_in
-
+@test
 def greeting():
     print("안녕하세요~")
     #수정하진 않고 확장에는 열려있음
-#인수없으니까 바로 greeting?호출
-t=test(greeting)
+
+greeting()
+
+#개방폐쇄원칙
 
