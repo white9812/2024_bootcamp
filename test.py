f40afd778bec3819a6cd7e62fe532644ffbe7a3e
white9812 <white9812@gmail.com>
2024-01-19 18:59:08 +0900

    t1

---
+++ test.py
@@ -0,0 +1,27 @@
+# def make_a_sound(n):
+#     print("quak")
+#
+# # make_a_sound()
+# # a=make_a_sound()
+# # print(a)
+# make_a_sound(1)
+# def make_a_sound(n):
+#      return f"quak{n}"
+#
+# make_a_sound("여기")
+# a=make_a_sound()
+# print(make_a_sound())
+#
+# make_a_sound(1)
+# print(make_a_sound(1))
+# print(make_a_sound())
+#
+def echo(anything):
+     return anything +" "+ anything
+echo("아마")
+print(echo("아마")
+)
+def menu(wine,entree,dessert):
+     return {"wine":wine,"entree":entree,"dessert":dessert}
+menu("chardonnay","chicken","cake")
+print(menu("chardonnay","chicken","cake"))
