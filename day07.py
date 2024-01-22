306597aca88be86d8cd8057e4e34cca6f3dd2c56
white9812 <white9812@gmail.com>
2024-01-22 10:36:28 +0900

    v7.0 assignment

---
+++ day07.py
@@ -0,0 +1,45 @@
+class FlyingMixin:
+    def fly(self):
+        return f"{self.__name}이(가) 하늘을 훨훨 날아갑니다~"
+
+class SwimmingMixin:
+    def swim(self):
+        return f"{self.__name}이(가) 수영을 합니다."
+
+class Pokemon:
+    def __init__(self, name):
+        self.__name = name
+        self.hp=hp #instance
+
+    def attack(self):
+        print("공격~")
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name): #self가 오니까 바로 객체가 와야함
+        self.__name = new_name
+
+    #name = property(get_name, set_name)
+    def __str__(self):
+        return self.__name + " 입니다"
+
+    def  __add__(self,target):
+        #return self.__name+"+"target.__name
+        return f"두 포켓몬스터 체력의 합은 {self.hp+target.hp}입니다."
+
+
+class Charizard(Pokemon, FlyingMixin):
+    pass
+
+class Gyarados(Pokemon, SwimmingMixin):
+    pass
+
+g1 = Gyarados("갸라도스",100)
+c1 = Charizard("리자몽",100)
+print(g1)
+print(c1)
+# print(g1+c1)
+print()
