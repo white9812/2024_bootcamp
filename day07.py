c1c4b13ca073dcc443ff18abce418be85464b61c
white9812 <white9812@gmail.com>
2024-01-22 13:14:11 +0900

    v7.8 module

--- day07.py
+++ day07.py
@@ -1,5 +1,5 @@
 # module
-import mymath
+from mymath import *
 
 
 while True:
@@ -7,13 +7,13 @@ while True:
 
     if menu == '1':
         fahrenheit = float(input('Input Fahrenheit : '))
-        print(f'Fahrenheit : {fahrenheit}F, Celsius : {mymath.fahrenheit_to_celsius(fahrenheit):.4f}C')
+        print(f'Fahrenheit : {fahrenheit}F, Celsius : {fahrenheit_to_celsius(fahrenheit):.4f}C')
     elif menu == '2':
         celsius = float(input('Input Celsius : '))
-        print(f'Celsius : {celsius}C, Fahrenheit : {mymath.celsius_to_fahrenheit(celsius):.4f}F')
+        print(f'Celsius : {celsius}C, Fahrenheit : {celsius_to_fahrenheit(celsius):.4f}F')
     elif menu == '3':
         number = int(input("Input number : "))
-        if mymath.isprime(number):
+        if isprime(number):
             print(f'{number} is prime number')
         else:
             print(f'{number} is NOT prime number!')
@@ -26,7 +26,7 @@ while True:
             n1, n2 = n2, n1
 
         for number in range(n1, n2 + 1):
-            if mymath.isprime(number):
+            if isprime(number):
                 print(number, end=' ')
         print()
     elif menu == '5':
--- mymath.py
+++ mymath.py
@@ -1,22 +1,33 @@
 def isprime(n) -> bool:
     """
-       매개변수로 넘겨 받은 수가 소수인지 여부를 boolean으로 리턴
-       :param n: 판정할 매개변수
-       :return: 소수면 True, 소수가 아니면 False
-       """
+    A function that returns as a boolean whether the number passed as a parameter is a prime number.
+    :param n: Parameter to judge
+    :return: True if prime number, False if not prime number.
+    """
     if n < 2:
         return False
     else:
         i = 2
-        while i * i <= n:
+        while i*i <= n:
             if n % i == 0:
                 return False
             i += 1
         return True
 
-def fahrenheit_to_celsius->float():
-    """
 
-    :return:
+def fahrenheit_to_celsius(fahrenheit) -> float:
     """
-    return (fahrenheit-32.0)*5.0/9.0
+    Function to convert Fahrenheit temperature to Celsius temperature
+    :param fahrenheit:
+    :return: celsius temperature
+    """
+    return (fahrenheit - 32.0) * 5.0 / 9.0
+
+
+def celsius_to_fahrenheit(celsius) -> float:
+    """
+    Function to convert Celsius temperature to Fahrenheit temperature
+    :param celsius:
+    :return: fahrenheit temperature
+    """
+    return (celsius*9.0/5.0)+32.0
